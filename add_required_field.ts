0100853d8dfa18f728e775f2758b795c81a16519
kobelb <brandon.kobel@elastic.co>
2023-03-31 08:19:03 -0400

    Fixing adding a required field, need an intermediate version

--- add_required_field.ts
+++ add_required_field.ts
@@ -23,6 +23,52 @@ coreSetup.savedObjects.registerType({
 // Kibana N + 1
 interface MyTypeDataModel {
     foo: string
+    bar?: number // This has to be optional because we can't consistently search on this until the backfill has ran
+}
+
+coreSetup.savedObjects.registerType({
+    name: 'my-type',
+    mappings: {
+        properties: {
+            foo: { type: 'keyword' },
+            bar: { type: 'integer' }
+        },
+    },
+    versions: {
+        0: {
+            schema: schema.object({
+                foo: schema.string()
+            }, { unknowns: 'ignore' }),
+        },
+        1: {
+            schema: schema.object({
+                foo: schema.string(),
+                bar: schema.maybe(schema.number()),
+            }, { unknowns: 'ignore' }),
+            backfill: true,
+            // only ran when the doc version is < 1
+            up: (doc, ctx) => {
+                const { bar, ...attrs } = doc;
+                return {
+                    ...attrs,
+                    bar: bar || 1,
+                };
+            },
+
+            onWrite: (doc, ctx) => {
+                const { bar, ...attrs } = doc;
+                return {
+                    ...attrs,
+                    bar: bar || 1,
+                };
+            },
+        },
+    }
+});
+
+// Kibana N + 2
+interface MyTypeDataModel {
+    foo: string
     bar: number
 }
 
@@ -43,16 +89,31 @@ coreSetup.savedObjects.registerType({
         1: {
             schema: schema.object({
                 foo: schema.string(),
-                bar: schema.number(),
+                bar: schema.maybe(schema.number()),
             }, { unknowns: 'ignore' }),
+            backfill: true,
             // only ran when the doc version is < 1
             up: (doc, ctx) => {
                 const { bar, ...attrs } = doc;
                 return {
                     ...attrs,
-                    bar: 1,
+                    bar: bar || 1,
                 };
             },
+
+            onWrite: (doc, ctx) => {
+                const { bar, ...attrs } = doc;
+                return {
+                    ...attrs,
+                    bar: bar || 1,
+                };
+            },
+        },
+        2: {
+            schema: schema.object({
+                foo: schema.string(),
+                bar: schema.number(),
+            }, { unknowns: 'ignore' }),
         },
     }
 });
